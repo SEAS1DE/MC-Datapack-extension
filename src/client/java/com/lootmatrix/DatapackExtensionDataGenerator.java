4f2d19eec0692a7e95fe01e63551132832f10ace
SEAS1DE <qq1602770686@outlook.com>
2026-02-02 13:52:26 +0800

    initial commit

---
+++ src/client/java/com/lootmatrix/DatapackExtensionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lootmatrix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DatapackExtensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
